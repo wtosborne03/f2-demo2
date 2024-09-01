f073d6b2436d03742883f1dc2f184ad53cc15ca7
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2024-09-01 19:58:01 -0400

    compressor 2

--- src/pages/prompt_photo.svelte
+++ src/pages/prompt_photo.svelte
@@ -4,7 +4,7 @@
   import type { PlayerState } from "../types/player_state";
   import type { PromptData } from "../types/page_data";
   import { player_state } from "../stores/player_state";
-  import Compressor from "compressorjs";
+  import imageCompression from "browser-image-compression";
 
   let m_data: PromptData;
   m_data = get<PlayerState>(player_state).page_data;
@@ -12,23 +12,21 @@
   let base64Image: string | null = null;
   let fileinput: HTMLInputElement;
 
-  function handleFileInput(event: Event) {
+  async function handleFileInput(event: Event) {
     const fileInput = event.target as HTMLInputElement;
     const file = fileInput.files?.[0];
 
     if (file) {
-      new Compressor(file, {
-        convertSize: 100000,
-        quality: 0.6,
-        success(result) {
-          const reader = new FileReader();
-          reader.onload = (e: ProgressEvent<FileReader>) => {
-            base64Image = e.target?.result as string;
-            submit_prompt(base64Image);
-          };
-          reader.readAsDataURL(result);
-        },
-      });
+      const options = {
+        maxSizeMB: 0.1,
+      };
+      const compressedFile = await imageCompression(file, options);
+      const reader = new FileReader();
+      reader.onload = (e: ProgressEvent<FileReader>) => {
+        base64Image = e.target?.result as string;
+        submit_prompt(base64Image);
+      };
+      reader.readAsDataURL(compressedFile);
     }
   }
 
