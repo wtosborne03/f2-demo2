907b6c6bbe45536a875b44104d7a59fe8d329d41
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2024-09-01 20:01:29 -0400

    upd

--- src/pages/prompt_photo.svelte
+++ src/pages/prompt_photo.svelte
@@ -5,14 +5,17 @@
   import type { PromptData } from "../types/page_data";
   import { player_state } from "../stores/player_state";
   import imageCompression from "browser-image-compression";
+  import { stat } from "fs";
 
   let m_data: PromptData;
   m_data = get<PlayerState>(player_state).page_data;
 
   let base64Image: string | null = null;
   let fileinput: HTMLInputElement;
+  let statuse = "";
 
   async function handleFileInput(event: Event) {
+    statuse = "Compressing Image...";
     const fileInput = event.target as HTMLInputElement;
     const file = fileInput.files?.[0];
 
@@ -21,6 +24,8 @@
         maxSizeMB: 0.1,
       };
       const compressedFile = await imageCompression(file, options);
+      statuse = "Compressing Image... Done";
+      statuse = "File size: " + compressedFile.size / 1024 / 1024 + " MB";
       const reader = new FileReader();
       reader.onload = (e: ProgressEvent<FileReader>) => {
         base64Image = e.target?.result as string;
@@ -46,6 +51,7 @@
 >
   <div>Select Photo:</div>
   <div class="mb-2 p-4">{m_data.question}</div>
+  {statuse}
   <button class="btn variant-filled" on:click={() => fileinput.click()}
     >Choose</button
   >
