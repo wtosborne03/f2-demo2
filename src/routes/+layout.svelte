1f55f21ceea720717e9a7a9e0b57c035a02712f2
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2024-09-03 15:56:01 -0400

    timer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,18 +16,48 @@
   import { storePopup } from "@skeletonlabs/skeleton";
   storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
-  import { player_state } from "../stores/player_state";
+  import { player_state } from "../stores/player_state"; // Assuming you have a timer store
 
   import doubloon from "$lib/assets/icons/doubloon.png";
 
   let score = 0;
   let name = "";
   let admin = false;
+
+  let timer_duration = 0;
+  let time_left = 0;
+  let timer_running = false;
+  let timer: any;
+
+  function startTimer(duration: number) {
+    timer_running = true;
+    timer_duration = duration;
+    time_left = duration;
+    timer = setInterval(() => {
+      time_left--;
+      if (time_left <= 0) {
+        clearInterval(timer);
+        timer_running = false;
+      }
+    }, 1000);
+  }
+
+  function stopTimer() {
+    timer_running = false;
+    clearInterval(timer);
+  }
+
   player_state.subscribe((value: PlayerState) => {
     score = value.score;
     name = value.name;
     admin = value.admin;
+    if (value.timer > 0) {
+      startTimer(value.timer);
+    } else {
+      stopTimer();
+    }
   });
+
   initializeStores();
 </script>
 
@@ -56,6 +86,16 @@
           <img class="w-8 h-8" src={doubloon} alt="coin" /></span
         >
       </svelte:fragment>
+
+      <!-- Countdown Bar -->
+      {#if timer_running}
+        <div class="w-full bg-gray-200 h-2 mt-2 col-span-3">
+          <div
+            class="bg-blue-500 h-full"
+            style="width: {(100 * time_left) / timer_duration}%"
+          ></div>
+        </div>
+      {/if}
     </AppBar>
   </svelte:fragment>
   <!-- Page Route Content -->
