fc1ed0c6b89a8e5121eb4943f87c292b584295e3
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2024-09-03 17:08:29 -0400

    sticky 2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -63,42 +63,43 @@
 
 <Toast />
 <!-- App Shell -->
-<div class="fixed w-full z-10">
-  <!-- App Bar -->
-  <AppBar
-    gridColumns="grid-cols-3"
-    slotDefault="place-self-center"
-    slotTrail="place-content-end"
-  >
-    <svelte:fragment slot="lead">
-      <strong class="text-xl uppercase">JG</strong>
-    </svelte:fragment>
-    <span class="text-xl flex flex-col justify-center items-center gap-0"
-      >{name}{#if admin}
-        <span class="text-sm">[admin]</span>
-      {/if}</span
+<div class="sticky top-0 h-24 w-full">
+  <div class="absolute inset-x-0">
+    <!-- App Bar -->
+    <AppBar
+      gridColumns="grid-cols-3"
+      slotDefault="place-self-center"
+      slotTrail="place-content-end"
     >
-
-    <svelte:fragment slot="trail">
-      <span class="text-xl flex flex-row items-center justify-center gap-2"
-        >{score}
-        <img class="w-8 h-8" src={doubloon} alt="coin" /></span
+      <svelte:fragment slot="lead">
+        <strong class="text-xl uppercase">JG</strong>
+      </svelte:fragment>
+      <span class="text-xl flex flex-col justify-center items-center gap-0"
+        >{name}{#if admin}
+          <span class="text-sm">[admin]</span>
+        {/if}</span
       >
-    </svelte:fragment>
 
-    <!-- Countdown Bar -->
-    {#if timer_running}
-      <div class="w-screen bg-gray-200 h-2 mt-2 col-span-3">
-        <div
-          class="bg-blue-500 h-full"
-          style="width: {(100 * time_left) / timer_duration}%"
-        ></div>
-      </div>
-    {/if}
-  </AppBar>
+      <svelte:fragment slot="trail">
+        <span class="text-xl flex flex-row items-center justify-center gap-2"
+          >{score}
+          <img class="w-8 h-8" src={doubloon} alt="coin" /></span
+        >
+      </svelte:fragment>
+
+      <!-- Countdown Bar -->
+      {#if timer_running}
+        <div class="w-screen bg-gray-200 h-2 mt-2 col-span-3">
+          <div
+            class="bg-blue-500 h-full"
+            style="width: {(100 * time_left) / timer_duration}%"
+          ></div>
+        </div>
+      {/if}
+    </AppBar>
+  </div>
 </div>
 
-<div class="h-24"></div>
 <!-- Page Route Content -->
 <div class="flex-grow">
   <slot />
