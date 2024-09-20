957c2800f2f63441995e238719521b7fa72e7f86
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2024-09-20 10:38:51 -0400

    formatting overflow

--- src/pages/vote.svelte
+++ src/pages/vote.svelte
@@ -19,18 +19,18 @@
   }
 </script>
 
-<div
-  class="container h-full mx-auto w-full flex flex-col justify-center items-center p-4"
->
-  <div class="mb-2 p-4">Choose the best answer:</div>
-  {#each m_data.options as answer}
-    <button
-      style="width: 100%; font-size: 1.5rem; white-space: normal; word-wrap: break-word;"
-      class="btn variant-filled m-2 p-2"
-      on:click={() =>
-        submit_answer(m_data.options.findIndex((a) => a == answer))}
-    >
-      {answer}
-    </button>
-  {/each}
+<div class="container w-full block flex-col p-2 overflow-scroll">
+  <div class="flex flex-col justify-center items-center">
+    <div class="mb-2 p-4">Choose the best answer:</div>
+    {#each m_data.options as answer}
+      <button
+        style="width: 100%; font-size: 1.5rem; white-space: normal; word-wrap: break-word;"
+        class="rounded-xl variant-filled m-2 p-2"
+        on:click={() =>
+          submit_answer(m_data.options.findIndex((a) => a == answer))}
+      >
+        {answer}
+      </button>
+    {/each}
+  </div>
 </div>
