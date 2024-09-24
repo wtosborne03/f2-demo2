f4348c69462fb7c249576f452a9bcc79c668a9e7
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2024-09-24 19:12:40 -0400

    sd

--- src/pages/shake_game.svelte
+++ src/pages/shake_game.svelte
@@ -91,7 +91,7 @@
         Math.pow(acceleration.x || 0, 2) + Math.pow(acceleration.y || 0, 2),
       );
       totalShakingDistance += distanceMoved;
-      sendProgress(event.acceleration?.y || 0);
+      sendProgress(event.acceleration?.z || 0);
     }
   }
 </script>
