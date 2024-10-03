27d91c5332a8b34ecf7d82b5c147ac0d97785843
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2024-10-03 19:13:03 -0400

    fix

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = "https://beqempvcsbabatjpqhxl.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJlcWVtcHZjc2JhYmF0anBxaHhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc5Nzc1MzIsImV4cCI6MjA0MzU1MzUzMn0.6KYGe2iBS5OicocyaTACkyRk-N-0glzgIRtD_hT4H54"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
