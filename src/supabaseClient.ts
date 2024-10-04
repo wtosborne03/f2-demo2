51e74c507543110453fdeaed646a44c4c543f814
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2024-10-04 12:01:26 -0400

    spin

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient, type SupabaseClientOptions } from '@supabase/supabase-js'
 
 const supabaseUrl = "https://beqempvcsbabatjpqhxl.supabase.co"
 const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJlcWVtcHZjc2JhYmF0anBxaHhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc5Nzc1MzIsImV4cCI6MjA0MzU1MzUzMn0.6KYGe2iBS5OicocyaTACkyRk-N-0glzgIRtD_hT4H54"
+
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
