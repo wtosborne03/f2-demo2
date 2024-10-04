0e8d4e4f5c2d52834a01f42cd135ec05fae43dad
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2024-10-04 13:02:42 -0400

    ui2

--- src/pages/game.svelte
+++ src/pages/game.svelte
@@ -1,10 +1,29 @@
 <script lang="ts">
+  import { drawerSettings } from "$lib/drawer";
   import { playerEmote } from "$lib/player_emote";
+  import { authStore } from "$lib/stores/authStore";
+  import { getDrawerStore } from "@skeletonlabs/skeleton";
+
+  const drawerStore = getDrawerStore();
 </script>
 
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-static-element-interactions -->
 <div
-  class="container h-full mx-auto flex justify-center items-center"
+  class="container h-full mx-auto flex flex-col justify-center items-center"
   on:click={playerEmote}
 >
-  Joined Game
+  <div class="text-xl">Joined Game 👍</div>
+
+  {#if $authStore.user}
+    <div class=""></div>
+  {:else}
+    <div class="mt-16 opacity-75">
+      (
+      <span
+        class="cursor-pointer text-blue-500 hover:text-blue-600"
+        on:click={() => drawerStore.open(drawerSettings)}>Sign In</span
+      > to customize avatar.)
+    </div>
+  {/if}
 </div>
