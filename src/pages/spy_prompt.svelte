ec1e3bb55c460bcc7637dea513e5f6812ff41837
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2024-10-24 14:35:25 -0400

    spy

---
+++ src/pages/spy_prompt.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import { sendMessage } from "$lib";
+  import { get } from "svelte/store";
+  import type { PlayerState } from "../types/player_state";
+  import type { PromptData } from "../types/page_data";
+  import { player_state } from "../stores/player_state";
+
+  let m_data: PromptData;
+  m_data = get<PlayerState>(player_state).page_data;
+
+  let answer_text = "";
+
+  function submit_prompt() {
+    sendMessage({
+      type: "game",
+      data: {
+        type: "answer",
+        answer: answer_text,
+      },
+    });
+  }
+</script>
+
+<div
+  class="container h-full mx-auto w-full flex flex-col justify-center items-center"
+>
+  <div>You're the SPY 🥷</div>
+  <div class="mb-2 p-4">{m_data.question}</div>
+  <form class="flex flex-col justify-center items-center">
+    <input
+      class="input w-full"
+      type="text"
+      maxlength="28"
+      bind:value={answer_text}
+      on:submit={submit_prompt}
+    />
+    <button class="btn variant-filled mt-12" on:click={submit_prompt}
+      >Submit</button
+    >
+  </form>
+</div>
---
+++ src/pages/spy_vote.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { sendMessage } from "$lib";
+  import { get } from "svelte/store";
+  import type { PlayerState } from "../types/player_state";
+  import type { SpyVoteData, VoteData } from "../types/page_data";
+  import { player_state } from "../stores/player_state";
+
+  let m_data: SpyVoteData;
+  m_data = get<PlayerState>(player_state).page_data;
+
+  function submit_answer(index: number) {
+    sendMessage({
+      type: "game",
+      data: {
+        type: "vote",
+        answer: m_data.options[index],
+      },
+    });
+  }
+</script>
+
+<div class="container w-full block flex-col p-2 overflow-scroll">
+  <div class="flex flex-col justify-center items-center">
+    <div class="mb-2 p-4">Who do you think is the SPY?</div>
+    {#each m_data.options as answer, index}
+      <button
+        style="width: 100%; font-size: 1.5rem; white-space: normal; word-wrap: break-word;"
+        class="rounded-xl variant-filled m-2 p-2"
+        on:click={() =>
+          submit_answer(m_data.options.findIndex((a) => a == answer))}
+      >
+        <div class="text-center text-sm">{m_data.option_by[index]}</div>
+        {answer}
+      </button>
+    {/each}
+  </div>
+</div>
