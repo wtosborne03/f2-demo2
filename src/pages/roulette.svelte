6563129fc197f813dd9f1c46be63b289b67868d0
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2024-11-13 18:17:14 -0500

    stuff

--- src/pages/roulette.svelte
+++ src/pages/roulette.svelte
@@ -3,7 +3,7 @@
   import { get } from "svelte/store";
   import type { PlayerState } from "../types/player_state";
   import { getToastStore } from "@skeletonlabs/skeleton";
-  import { sendMessage } from "$lib";
+  import { getName, sendMessage } from "$lib";
   import type { RouletteData } from "../types/page_data";
   import { RangeSlider } from "@skeletonlabs/skeleton";
 
@@ -25,7 +25,25 @@
     "Take 20 drinks": "Take 20 drinks",
     "Down your drink": "Down your drink",
     "Give the player on the right your wallet": "Give Wallet",
-    "Give the player on the left a nice compliment": "Compliment",
+    "Give the player on the left a nice compliment": "Compliment 👈",
+    "Take a shot": "Take a shot",
+    "Take off an article of clothing": "Clothes off",
+    "Shotgun a beer": "Shotgun Beer",
+    "Name a law that you have broken": "Name Crime",
+    "Text a family member that you died": "Text Family",
+    "Take 7 drinks if your'e unemployed": "Unemployed Drink",
+    "Take 8 drinks if you're a Man": "Men Drink",
+    "Take 8 drinks if you're a Woman": "Women Drink",
+    "Name your favorite slur": "Favorite Slur",
+    "Take a shot if you're an Atheist": "Atheists Drink",
+    "Show the last photo in your camera roll": "Camera Roll",
+    "Let the person on your right text anyone on your phone": "Text 'em",
+    "Reveal the first three digits of your Social Security Number":
+      "SSN sneak peek",
+    [`Take a drink from ${getName()}'s cup`]: `${getName()}'s cup`,
+    "Take a drink if you've lost money gambling": "Gamble Regret",
+    [`Give ${getName()} a compliment'`]: `Compliment Me`,
+    "Reveal your phone's daily screentime": "Screen Reveal",
   };
 
   const toastStore = getToastStore();
@@ -59,7 +77,7 @@
   <h1 class="text-4xl font-bold mb-8">Add a punishment to the Wheel</h1>
   {#each available_challenges as challenge}
     <button
-      class={"btn variant-filled w-full mb-4 text-xl font-medium" +
+      class={"btn variant-filled w-full mb-4 text-xl font-medium text-wrap" +
         (selected_challenge === challenge
           ? " variant-filled-primary outline-4 outline-white"
           : "")}
