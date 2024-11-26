7b9680be4eb6c02d220a55d2646815e4b3f7b1e0
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2024-11-26 14:08:11 -0500

    error

--- src/lib/auth/auth_box.svelte
+++ src/lib/auth/auth_box.svelte
@@ -19,6 +19,9 @@
     const { error } = await supabase.auth.signOut();
     if (error) {
       console.error("Error signing out:", error.message);
+      toastStore.trigger({
+        message: "Error signing out: " + error.message,
+      });
     } else {
       console.log("User signed out");
     }
