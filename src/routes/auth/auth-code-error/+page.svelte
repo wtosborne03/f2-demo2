46dc2be7717aaa7cee4c8eb0ed2548333d1e1af6
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2024-12-02 10:58:35 -0500

    error code

---
+++ src/routes/auth/auth-code-error/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { page } from "$app/stores";
+
+  $: errorMessage = $page.url.searchParams.get("message");
+</script>
+
+<h1>Error</h1>
+<p>{errorMessage}</p>
--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -1,21 +1,24 @@
 import { redirect } from '@sveltejs/kit';
 
-export const GET = async (event) => {
+export const GET = async (event: any) => {
   const {
     url,
     locals: { supabase }
   } = event;
+
   const code = url.searchParams.get('code') as string;
   const next = url.searchParams.get('next') ?? '/';
 
   if (code) {
-    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
       throw redirect(303, `/${next.slice(1)}`);
+    } else {
+      // Redirect to the error page with the error message
+      throw redirect(303, `/auth/auth-code-error?message=${encodeURIComponent(error.message)}`);
     }
-    console.log(error);
   }
 
-  // return the user to an error page with instructions
-  throw redirect(303, '/auth/auth-code-error');
+  // If no code is provided, redirect to the error page
+  throw redirect(303, '/auth/auth-code-error?message=Authorization code is missing');
 };
