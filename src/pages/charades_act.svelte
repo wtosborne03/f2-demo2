62140c7662041c518e8d0deda08a3865e19a0a2a
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2024-12-03 16:20:32 -0500

    charades

--- src/pages/charades_act.svelte
+++ src/pages/charades_act.svelte
@@ -13,7 +13,7 @@
 <div
   class="container h-full mx-auto w-full flex flex-col justify-center items-center text-center"
 >
-  <h3>Give a good argument as to why players should support your side</h3>
+  <h3 class="text-2xl mb-6">Act out your prompt</h3>
   <button
     style="font-size: 1.5rem;"
     class="btn variant-filled-error m-2 p-4"
