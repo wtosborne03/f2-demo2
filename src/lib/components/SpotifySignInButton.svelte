78a9ba52464a273045d0473b0f8d8dfb96e12466
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2025-12-12 20:25:43 -0500

    buttons

--- src/lib/components/SpotifySignInButton.svelte
+++ src/lib/components/SpotifySignInButton.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
     import Icon from "@iconify/svelte";
 
-  export let onClick: () => {};
+    export let onClick: () => {};
 </script>
 
 <button
-  aria-label="Continue with Spotify"
-  class="flex items-center justify-center h-12 bg-[#1abb53] px-2 py-1.5 rounded-md pr-3"
-  on:click={onClick}
+    aria-label="Continue with Spotify"
+    class="flex items-center justify-between h-12 bg-[#1abb53] px-2 py-1.5 rounded-md pr-3"
+    on:click={onClick}
 >
-  <Icon icon="mdi:spotify" font-size="1.75rem" class="mr-2 text-white" />
-  <span class="text-[1rem] text-white font-medium">Continue with Spotify</span>
+    <Icon icon="mdi:spotify" font-size="2rem" class="mr-2 text-white" />
+    <span class="text-[1rem] text-white font-medium">Continue with Spotify</span
+    >
 </button>
