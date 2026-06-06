cc17655f285ff02d506e6ac6b538303d7041eee0
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2026-06-06 15:32:29 -0400

    spy page

--- src/pages/spy.svelte
+++ src/pages/spy.svelte
@@ -1,35 +1,34 @@
 <script lang="ts">
-  import { get } from "svelte/store";
-  import type { PlayerState } from "../types/player_state";
-  import type { SpyVoteData, VoteData } from "../types/page_data";
-  import { gameClient, gameState } from "$lib/wsapi/gameClient";
+    import { get } from "svelte/store";
+    import type { SpyVoteData } from "../types/page_data";
+    import { gameClient, gameState } from "$lib/wsapi/gameClient";
+    import { Button } from "m3-svelte";
 
-  let m_data: SpyVoteData;
-  m_data = get(gameState).page_data;
+    let m_data: SpyVoteData;
+    m_data = get(gameState).page_data;
 
-  function submit_answer(index: number) {
-    gameClient.sendInput({
-      type: "playerVoteData",
-      answer: m_data.options[index],
-    });
-  }
+    function submit_answer(index: number) {
+        gameClient.sendInput({
+            type: "playerVoteData",
+            answer: m_data.options[index],
+        });
+    }
 </script>
 
 <div class="container w-full block flex-col p-2">
-  <div class="flex flex-col justify-center items-center">
-    <div class="mb-2 p-4 text-center">
-      You're the spy. Vote on someone else to misdirect other players.
+    <div class="flex flex-col justify-center items-center">
+        <div class="mb-2 p-4 text-center">
+            You're the spy. Vote on someone else to misdirect other players.
+        </div>
+        {#each m_data.options as answer, index}
+            <Button
+                size="l"
+                style="width:100%; margin-bottom: 1rem; margin-top: 1rem;"
+                onclick={() => submit_answer(m_data.options.findIndex((a) => a == answer))}
+            >
+                <div class=" text-xl mr-auto">{m_data.option_by[index]}</div>
+                {answer}
+            </Button>
+        {/each}
     </div>
-    {#each m_data.options as answer, index}
-      <button
-        style="width: 100%; font-size: 1.5rem; white-space: normal; word-wrap: break-word;"
-        class="rounded-xl preset-filled m-2 p-2"
-        on:click={() =>
-          submit_answer(m_data.options.findIndex((a) => a == answer))}
-      >
-        <div class="text-center text-sm">{m_data.option_by[index]}</div>
-        {answer}
-      </button>
-    {/each}
-  </div>
 </div>
