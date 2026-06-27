7f2005c606803fe2fcbfcccb5105807eb5c6faa8
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2026-06-26 20:29:28 -0400

    k

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -2,10 +2,15 @@ import { get, writable } from 'svelte/store';
 import { createAuthClient } from 'better-auth/svelte';
 import { magicLinkClient } from 'better-auth/client/plugins';
 import { magicLink } from 'better-auth/plugins/magic-link';
+import { withCapacitor } from 'better-auth-capacitor/client';
 
-export const authClient = createAuthClient({
-    baseURL: import.meta.env.VITE_PUBLIC_API_URL,
-    fetchOptions: { credentials: 'include' },
-    plugins: [
-    ]
-});
+export const authClient = createAuthClient(
+    withCapacitor({
+        baseURL: import.meta.env.VITE_PUBLIC_API_URL,
+        fetchOptions: { credentials: 'include' },
+        plugins: [
+        ]
+    }, {
+        scheme: 'couchcup',
+    })
+);
