7bab84022ddd0fc9dbfa9c51e6d6dcf2fc569d36
wtosborne03 <53115379+wtosborne03@users.noreply.github.com>
2026-07-16 14:13:21 -0400

    w

--- src/pages/vote.svelte
+++ src/pages/vote.svelte
@@ -13,15 +13,18 @@
   }
 </script>
 
-<div class="flex flex-col justify-center items-center h-full w-full max-w-md mx-auto px-6 py-6 text-center">
+<div
+  class="flex flex-col justify-center items-center h-full w-full max-w-md mx-auto px-6 py-6 text-center"
+>
   <div class="text-2xl font-black mb-6">Choose the best answer:</div>
-  
+
   <div class="w-full flex flex-col gap-4">
     {#each m_data.options as answer}
       <button
         type="button"
-        class="btn btn-outline btn-primary btn-lg w-full py-5 h-auto text-lg font-black leading-snug"
-        onclick={() => submit_answer(m_data.options.findIndex((a) => a == answer))}
+        class="btn btn-primary btn-lg w-full py-5 h-auto text-lg font-black leading-snug"
+        onclick={() =>
+          submit_answer(m_data.options.findIndex((a) => a == answer))}
       >
         {answer}
       </button>
